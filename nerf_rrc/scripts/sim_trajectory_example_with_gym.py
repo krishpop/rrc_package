72618d2d52ceffd82cc21b493b989fb2358fa9ab
krishnan <krshna@stanford.edu>
2022-06-07 23:18:22 -0700

    updated sim traj

--- nerf_rrc/scripts/sim_trajectory_example_with_gym.py
+++ nerf_rrc/scripts/sim_trajectory_example_with_gym.py
@@ -6,14 +6,18 @@ a dummy policy.
 """
 import json
 import sys
+import numpy as np
 
 from nerf_rrc import cube_trajectory_env
 from nerf_rrc.pd_control import PDControlPolicy
+import finger_simulation.tasks.move_cube_on_trajectory as task
 
 
 def main():
     goal_json = sys.argv[1]
     goal = json.loads(open(goal_json, "r").read())["_goal"]
+    initial_pose = task.move_cube.Pose(position=np.array([-0.0023, 0.0013, 0.034]),
+            orientation=np.array([3.04629235e-03, 1.34290364e-02,  7.28152931e-01,  6.85276330e-01])
     env = cube_trajectory_env.SimCubeTrajectoryEnv(
         goal_trajectory=goal,  # passing None to sample a random trajectory
         action_type=cube_trajectory_env.ActionType.TORQUE,
